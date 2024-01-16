8b66f4a6deac91a58905970dd7aa12f8d91c7238
Ricardo Lucas <ricardo.lucas@turbinekreuzberg.com>
2024-01-16 14:36:12 +0000

    Add project to repository

---
+++ src/Pyz/Zed/MessageBroker/MessageBrokerDependencyProvider.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\MessageBroker;
+
+use Spryker\Zed\Asset\Communication\Plugin\MessageBroker\AssetMessageHandlerPlugin;
+use Spryker\Zed\MessageBroker\Communication\Plugin\MessageBroker\CorrelationIdMessageAttributeProviderPlugin;
+use Spryker\Zed\MessageBroker\Communication\Plugin\MessageBroker\TenantActorMessageAttributeProviderPlugin;
+use Spryker\Zed\MessageBroker\Communication\Plugin\MessageBroker\TimestampMessageAttributeProviderPlugin;
+use Spryker\Zed\MessageBroker\Communication\Plugin\MessageBroker\TransactionIdMessageAttributeProviderPlugin;
+use Spryker\Zed\MessageBroker\Communication\Plugin\MessageBroker\ValidationMiddlewarePlugin;
+use Spryker\Zed\MessageBroker\MessageBrokerDependencyProvider as SprykerMessageBrokerDependencyProvider;
+use Spryker\Zed\MessageBrokerAws\Communication\Plugin\MessageBroker\Receiver\HttpChannelMessageReceiverPlugin;
+use Spryker\Zed\MessageBrokerAws\Communication\Plugin\MessageBroker\Sender\HttpChannelMessageSenderPlugin;
+use Spryker\Zed\OauthClient\Communication\Plugin\MessageBroker\AccessTokenMessageAttributeProviderPlugin;
+use Spryker\Zed\Payment\Communication\Plugin\MessageBroker\PaymentCancelReservationFailedMessageHandlerPlugin;
+use Spryker\Zed\Payment\Communication\Plugin\MessageBroker\PaymentConfirmationFailedMessageHandlerPlugin;
+use Spryker\Zed\Payment\Communication\Plugin\MessageBroker\PaymentConfirmedMessageHandlerPlugin;
+use Spryker\Zed\Payment\Communication\Plugin\MessageBroker\PaymentMethodMessageHandlerPlugin;
+use Spryker\Zed\Payment\Communication\Plugin\MessageBroker\PaymentPreauthorizationFailedMessageHandlerPlugin;
+use Spryker\Zed\Payment\Communication\Plugin\MessageBroker\PaymentPreauthorizedMessageHandlerPlugin;
+use Spryker\Zed\Payment\Communication\Plugin\MessageBroker\PaymentRefundedMessageHandlerPlugin;
+use Spryker\Zed\Payment\Communication\Plugin\MessageBroker\PaymentRefundFailedMessageHandlerPlugin;
+use Spryker\Zed\Payment\Communication\Plugin\MessageBroker\PaymentReservationCanceledMessageHandlerPlugin;
+use Spryker\Zed\Product\Communication\Plugin\MessageBroker\ProductExportMessageHandlerPlugin;
+use Spryker\Zed\ProductReview\Communication\Plugin\MessageBroker\ProductReviewAddReviewsMessageHandlerPlugin;
+use Spryker\Zed\SearchHttp\Communication\Plugin\MessageBroker\SearchEndpointMessageHandlerPlugin;
+use Spryker\Zed\Session\Communication\Plugin\MessageBroker\SessionTrackingIdMessageAttributeProviderPlugin;
+use Spryker\Zed\TaxApp\Communication\Plugin\MessageBroker\TaxAppMessageHandlerPlugin;
+
+class MessageBrokerDependencyProvider extends SprykerMessageBrokerDependencyProvider
+{
+    /**
+     * @return array<\Spryker\Zed\MessageBrokerExtension\Dependency\Plugin\MessageSenderPluginInterface>
+     */
+    public function getMessageSenderPlugins(): array
+    {
+        return [
+            new HttpChannelMessageSenderPlugin(),
+        ];
+    }
+
+    /**
+     * @return array<\Spryker\Zed\MessageBrokerExtension\Dependency\Plugin\MessageReceiverPluginInterface>
+     */
+    public function getMessageReceiverPlugins(): array
+    {
+        return [
+            new HttpChannelMessageReceiverPlugin(),
+        ];
+    }
+
+    /**
+     * @return array<\Spryker\Zed\MessageBrokerExtension\Dependency\Plugin\MessageHandlerPluginInterface>
+     */
+    public function getMessageHandlerPlugins(): array
+    {
+        return [
+            new PaymentCancelReservationFailedMessageHandlerPlugin(),
+            new PaymentConfirmationFailedMessageHandlerPlugin(),
+            new PaymentConfirmedMessageHandlerPlugin(),
+            new PaymentPreauthorizationFailedMessageHandlerPlugin(),
+            new PaymentPreauthorizedMessageHandlerPlugin(),
+            new PaymentReservationCanceledMessageHandlerPlugin(),
+            new PaymentRefundedMessageHandlerPlugin(),
+            new PaymentRefundFailedMessageHandlerPlugin(),
+            new PaymentMethodMessageHandlerPlugin(),
+            new AssetMessageHandlerPlugin(),
+            new ProductExportMessageHandlerPlugin(),
+            new SearchEndpointMessageHandlerPlugin(),
+            new ProductReviewAddReviewsMessageHandlerPlugin(),
+            new TaxAppMessageHandlerPlugin(),
+        ];
+    }
+
+    /**
+     * @return array<\Spryker\Zed\MessageBrokerExtension\Dependency\Plugin\MessageAttributeProviderPluginInterface>
+     */
+    public function getMessageAttributeProviderPlugins(): array
+    {
+        return [
+            new CorrelationIdMessageAttributeProviderPlugin(),
+            new TimestampMessageAttributeProviderPlugin(),
+            new AccessTokenMessageAttributeProviderPlugin(),
+            new TransactionIdMessageAttributeProviderPlugin(),
+            new SessionTrackingIdMessageAttributeProviderPlugin(),
+            new TenantActorMessageAttributeProviderPlugin(),
+        ];
+    }
+
+    /**
+     * @return array<\Symfony\Component\Messenger\Middleware\MiddlewareInterface>
+     */
+    public function getMiddlewarePlugins(): array
+    {
+        return [
+            new ValidationMiddlewarePlugin(),
+        ];
+    }
+}
